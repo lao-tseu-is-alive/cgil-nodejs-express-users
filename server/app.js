34408ca1035255cf937d885301ee58cb65fd3af8
Carlos GIL <lao.tseu.is.alive@gmail.com>
2020-07-24 15:49:45 +0200

    Completed Hello World ExpressJS App, adding Skeleton css as static resource

--- server/app.js
+++ server/app.js
@@ -1,16 +1,20 @@
-
 const express = require('express');
+const path = require('path');
+const process = require('process')
 const bodyParser = require('body-parser');
 const welcome = require('./messages/welcome');
+require('./config/config');
 // var {User} = require('./models/user');
 // var {authenticate} = require('./middleware/authenticate');
-
+console.log('working directory :' + process.cwd())
+console.log('script  directory :' + __dirname)
 const app = express();
 
-
 app.use(bodyParser.json());
-
+app.use('/static',express.static(path.join(__dirname, 'public')));
 app.get('/', welcome);
 
-
 module.exports = app;
+
+app.listen(process.env.PORT,
+    () => console.log(`${process.env.MODULE_NAME} app listening at http://localhost:${process.env.PORT}`))
--- server/messages/welcome.js
+++ server/messages/welcome.js
@@ -1,3 +1,17 @@
 require('../config/config');
 
-module.exports = (_, res) => res.send(`Welcome to ${process.env.MODULE_NAME}`);
+module.exports = (_, res) => res.send(
+`<html>
+<head><title>Node Express Hello World</title></head>
+<body>
+<div class="container">
+    <h3>Welcome to ${process.env.MODULE_NAME}</h3>
+    <p>A Hello World App written with <a href="https://nodejs.org/en/">Node</a> using
+     <a href="https://expressjs.com/">ExpressJS</a></p>
+    </p>
+    <p>Formatting is done using the <a href="http://getskeleton.com/">Skeleton</a> css minimal framework</p>
+     <link rel="stylesheet" href="static/skeleton.min.css">
+</div>    
+</body> 
+</html>
+`);
